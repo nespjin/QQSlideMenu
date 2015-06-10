d9cef24611cc5616d20680ead7979cc8a9c7c465
xuxubin <675125417@qq.com>
2015-06-10 21:58:05 +0800

    增加view的自定义属性

--- app/src/main/java/com/sysu/qqslidemenu/SlidingScrollView.java
+++ app/src/main/java/com/sysu/qqslidemenu/SlidingScrollView.java
@@ -1,6 +1,7 @@
 package com.sysu.qqslidemenu;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.util.AttributeSet;
 import android.util.DisplayMetrics;
 import android.util.TypedValue;
@@ -19,6 +20,12 @@ import android.widget.LinearLayout;
  * 1、onMeasure:决定内部View的宽和高以及自己的宽和高
  * 2、onLayout: 决定子View放置的位置
  * 3、重写onTouchEvent方法
+ */
+
+/**
+ * 自定义属性：允许用户设置菜单离屏幕右边的边距
+ * 在attr.xml文件中增加属性
+ * 在使用view的地方增加命名空间和使用属性
  */
 
 public class SlidingScrollView extends HorizontalScrollView {
@@ -42,17 +49,47 @@ public class SlidingScrollView extends HorizontalScrollView {
      * @param attrs
      */
     public SlidingScrollView(Context context, AttributeSet attrs) {
-        super(context, attrs);
+        this(context, attrs, 0);
+    }
+
+    public SlidingScrollView(Context context) {
+        this(context, null);
+    }
+
+    /**
+     * 当使用了自定义属性是会调用此构造方法
+     * @param context
+     * @param attrs
+     * @param defStyleAttr
+     */
+    public SlidingScrollView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+
+        //获取自定义属性的值
+        TypedArray a = context.getTheme().obtainStyledAttributes(attrs,R.styleable.SlidingScrollView,defStyleAttr,0);
+        int n = a.getIndexCount();
+        for (int i=0;i<n;i++)
+        {
+            int attr = a.getIndex(i);
+            switch (attr)
+            {
+                case R.styleable.SlidingScrollView_rightPadding:
+                    //第二个参数时默认值
+                    mMenuRightPadding = a.getDimensionPixelSize(attr,
+                            (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,50,
+                                    context.getResources().getDisplayMetrics()));
+                    break;
+                default:
+                    break;
+            }
+        }
+        a.recycle();
 
         //获取屏幕尺寸
         WindowManager wm = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
         DisplayMetrics outMetrics = new DisplayMetrics();
         wm.getDefaultDisplay().getMetrics(outMetrics);
         mScreenWidth = outMetrics.widthPixels;
-
-        //把dp转化为px
-        mMenuRightPadding = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,50,
-                context.getResources().getDisplayMetrics());
     }
 
     @Override
